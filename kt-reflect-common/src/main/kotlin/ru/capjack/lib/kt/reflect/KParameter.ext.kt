7fdc915b5c49972bfbf9e4019ceb435be3942bcc
Alexey Chuprin <a.chuprin@capjack.ru>
2018-10-04 17:42:20 +0300

    Improve KParameter.ext

--- kt-reflect-common/src/main/kotlin/ru/capjack/lib/kt/reflect/KParameter.ext.kt
+++ kt-reflect-common/src/main/kotlin/ru/capjack/lib/kt/reflect/KParameter.ext.kt
@@ -3,4 +3,8 @@ package ru.capjack.lib.kt.reflect
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
 
+expect val KParameter.indexRef: Int
+
+expect val KParameter.nameRef: String?
+
 expect val KParameter.typeRef: KType
--- kt-reflect-jvm/src/main/kotlin/ru/capjack/lib/kt/reflect/KParameter.ext.kt
+++ kt-reflect-jvm/src/main/kotlin/ru/capjack/lib/kt/reflect/KParameter.ext.kt
@@ -3,5 +3,11 @@ package ru.capjack.lib.kt.reflect
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
 
+actual inline val KParameter.indexRef: Int
+	get() = index
+
+actual inline val KParameter.nameRef: String?
+	get() = name
+
 actual inline val KParameter.typeRef: KType
 	get() = type
