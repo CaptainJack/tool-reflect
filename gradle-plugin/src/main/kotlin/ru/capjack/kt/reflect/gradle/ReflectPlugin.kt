ba4d36404d672df8ea2c8a13b3f84e02120066f4
Alexey Chuprin <a.chuprin@capjack.ru>
2019-03-01 18:23:37 +0300

    Fix version resolution

--- gradle-plugin/src/main/kotlin/ru/capjack/kt/reflect/gradle/ReflectPlugin.kt
+++ gradle-plugin/src/main/kotlin/ru/capjack/kt/reflect/gradle/ReflectPlugin.kt
@@ -28,8 +28,8 @@ open class ReflectPlugin : Plugin<Project> {
 	}
 	
 	private fun configureDefaultVersionsResolutionStrategy(project: Project) {
-		project.configurations.forEach { configuration ->
-			configuration.resolutionStrategy.eachDependency(Action {
+		project.configurations.all {
+			resolutionStrategy.eachDependency(Action {
 				if (requested.group == ARTIFACT_GROUP && requested.name.startsWith("kt-reflect-") && requested.version.isNullOrEmpty()) {
 					useVersion(VERSION)
 				}
